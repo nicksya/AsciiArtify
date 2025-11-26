46d11df03d5c03d618858ece63de4a1a3df788b7
Mykyta Khomenko <mykyta.khomenko@gmail.com>
2025-11-26 17:12:27 +0100

    Update script for POC

--- doc/POC.sh
+++ doc/POC.sh
@@ -7,3 +7,6 @@ k get all -n argocd
 k get po -n argocd
 k port-forward svc/argocd-server -n argocd 8080:443&
 
+k get secret argocd-initial-admin-secret -n argocd -o jsonpath="{.data.password}" | base64 -d; echo
+
+
