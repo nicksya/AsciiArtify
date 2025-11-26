267e7c3888a09b57b4bb3849c4f8f537365659a3
Mykyta Khomenko <mykyta.khomenko@gmail.com>
2025-11-26 15:13:26 +0100

    Adding script for POC

---
+++ doc/POC.sh
@@ -0,0 +1,9 @@
+k3d cluster create argo
+kubectl cluster-info
+
+kubectl create namespace argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+k get all -n argocd
+k get po -n argocd
+k port-forward svc/argocd-server -n argocd 800:443&
+
